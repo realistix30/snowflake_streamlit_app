dccd495f9552bd7ab8d452fa3161bae24d9fb3b2
Andrew Justin Oconer <65805697+realistix30@users.noreply.github.com>
2023-10-10 15:15:19 +0800

    Update streamlit_app.py
    added new items

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,12 @@
 import streamlit
 streamlit.title('My parents new Healthy Diner')
-streamlit.header('Brekfast')
-streamlit.text('Eggs')
+
+
+streamlit.header('Breakfast Menu')
+streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞Avocado Toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
