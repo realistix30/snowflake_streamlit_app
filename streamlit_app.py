adeef540b3f540717d57b44df1afa65966cf757c
Andrew Justin Oconer <65805697+realistix30@users.noreply.github.com>
2023-10-10 15:17:20 +0800

    Update streamlit_app.py
    added new list

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,9 @@
 import streamlit
+import pandaas
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
+
 streamlit.title('My parents new Healthy Diner')
 
 
@@ -10,3 +15,4 @@ streamlit.text('🥑🍞Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
+streamlit.dataframe(my_fruit_list)
